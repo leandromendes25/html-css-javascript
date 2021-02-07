9ab223101627ae3c79a29183be4dc80be794b99a
leandromendes25 <leandromiendes@yahoo.com>
2021-02-07 19:34:30 -0300

    fixing js

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const Modal = {
+    open(){
+      document.querySelector('.modal-overlay')
+      .classList
+      .add('active')
+    },
+    close(){
+      document.querySelector('.modal-overlay')
+      .classList.remove('active')
+    }
+  }
+  const transacitons = [{}]
+
+  const Transaction = {
+    incomes() {
+    },
+    expenses() {
+
+    },
+    total() {
+
+    }
+  }
