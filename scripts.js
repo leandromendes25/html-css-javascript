19f66a6610f55464aa690695f795c575fbb911d8
leandromendes25 <leandromiendes@yahoo.com>
2021-02-09 11:32:15 -0300

    Transaction add

--- scripts.js
+++ scripts.js
@@ -9,7 +9,25 @@ const Modal = {
       .classList.remove('active')
     }
   }
-  const transacitons = [{}]
+  const transacitons = [{
+    id: 1,
+    description: 'Luz',
+    amount: - 50000,
+    date: '23/01/2021'
+  },
+  {
+    id: 2,
+    description: 'Criação de website',
+    amount: 500000,
+    date: '23/01/2021'
+  },
+  {
+    id: 3,
+    description: 'internet',
+    amount: - 20000,
+    date: '23/01/2021'
+  },
+]
 
   const Transaction = {
     incomes() {
